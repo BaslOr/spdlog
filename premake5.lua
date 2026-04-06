818eb3fd658a34f6affc2b3ccd67845647e63520
BaslOr <bastian.schwarzmann@web.de>
2026-04-06 17:22:58 +0200

    Added Premake file

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "spdlog"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "include/**.h",
+        "src/**.cpp"
+    }
+
+    includedirs {
+        "include"
+    }
+
+    defines {
+        "SPDLOG_COMPILED_LIB"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        defines { "_CRT_SECURE_NO_WARNINGS" }
+
+    filter "system:linux"
+        links { "pthread" }
+
+    filter "system:macosx"
+        -- no extra links needed
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter {}
